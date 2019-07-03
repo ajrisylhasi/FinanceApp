d394710568a02d72374ae7a752b18b6c7202fd59
Ajri Sylhasi <ajrisylhasi@gmail.com>
2019-07-03 11:15:31 +0200

    Dishka me app.rb

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module DbTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
       html_tag
     }
