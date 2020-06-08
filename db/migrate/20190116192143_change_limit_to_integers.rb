a4b6ee6b157afd4fea6b347ca766c050b21282a2
Ajri Sylhasi <ajrisylhasi@gmail.com>
2020-06-08 15:13:21 +0200

    Trial with truncate

--- db/migrate/20190116192143_change_limit_to_integers.rb
+++
@@ -1,6 +0,0 @@
-class ChangeLimitToIntegers < ActiveRecord::Migration[5.2]
-  def change
-    change_column :articles, :tarif_kodi, :integer, :limit => 8
-    change_column :products, :tarif_kodi, :integer, :limit => 8
-  end
-end
